c59f446079201e3d5c5f8fedd4639775cacf2b86
tannerhadler-ux <tannerhadler@gustavus.edu>
2026-03-08 18:05:59 -0500

    created Class

---
+++ src/Class.java
@@ -0,0 +1,54 @@
+import java.util.List;
+
+//This class was coded by Tanner Hadler
+public class Class {
+	private String className;
+	private String classDescription;
+	private String department;
+	private int level;
+	private String time;
+	private List<String> classPreReqs;
+	private int semester;
+	private String professor;
+	private boolean dayOrNight;
+	
+	// to initialize Class
+	public Class(
+			String className,
+			String classDescription,
+			String department,
+			int level,
+			String time,
+			List<String> classPreReqs,
+			int semester,
+			String professor,
+			boolean dayOrNight) {
+		this.className = className;
+		this.classDescription = classDescription;
+		this.department = department;
+		this.level = level;
+		this.time = time;
+		this.classPreReqs = classPreReqs;
+		this.semester = semester;
+		this.professor = professor;
+		this.dayOrNight = dayOrNight;
+	}
+	
+	public String getClassName() {
+		//this returns the name of the class
+		return this.className;
+	}
+	
+	public int updateCredits() {
+		//this will add this course to the student's total credits (cannot be completed until Student is done
+		//Students.credits += 1;
+		//return Students.credits;
+		return level;
+	}
+	
+	public static void main(String[] args) {
+		List<String> softwarePreReqs = List.of("Systems 1", "Systems 2");
+		Class softwareEngineering = new Class("Software Engineering", "teaching how to engineer software", "MCS", 300, "12:00-1:35", softwarePreReqs, 2026, "Louis Yu", true);
+		softwareEngineering.getClassName();
+	}
+}
