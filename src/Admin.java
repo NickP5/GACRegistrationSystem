b7d1a8901bf8587e74d1960347fc0b0780e97643
pyakimova-sudo <pyakimova@gustavus.edu>
2026-03-08 14:12:24 -0500

    Update Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,5 +1,54 @@
 
 // Pavla Yakimova coded this class
 public class Admin {
+	
+	int adminID;
+	String username;
+	String password;
+	String classes;
+	
+	// Initialize constructor
+	public Admin(Integer adminID,String username, String password, String classes) {
+		this.adminID = adminID;
+		this.username = username;
+		this.password = password;
+		this.classes = classes;
+	}
 
+	
+	// Methods
+	public void changePassword() {
+		
+	}
+	
+	public void createClass() {
+		
+	}
+	
+	public void editClass() {
+		
+	}
+	
+	public void addStudent() {
+		
+	}
+	
+	public void removeStudent() {
+		
+	}
+	
+	public void updateMajor() {
+		
+	}
+	
+	public void updateMinot() {
+		
+	}
+	
+	public void editStudent() {
+		
+	}
+	
+	
+	
 }
